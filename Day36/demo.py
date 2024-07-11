4a9676c1a8f07f72df0cb574729dedb4dafaa570
r-anjesh <anjeshsa12345@gmail.com>
2024-07-11 11:22:28 +0530

    security improvement

--- Day36/demo.py
+++ Day36/demo.py
@@ -2,10 +2,10 @@ import requests
 
 STOCK = "TSLA"
 COMPANY_NAME = "Tesla Inc"
-STOCK_API_KEY = "4B0WP1H6NP2XU2ZA"
-NEWS_API_KEY = "238de636843c431f861e9b28dfaf48e2"
-ACCOUNT_SID = "AC7040ee5722e907c65c2d3a9f0db911d9"
-AUTH_TOKEN = "c04e31caf1d87cbe3c49244230cd4089"
+STOCK_API_KEY = "*********"
+NEWS_API_KEY = "*************"
+ACCOUNT_SID = "********"
+AUTH_TOKEN = "**********"
 
 parameter = {
     "function": "TIME_SERIES_DAILY",
